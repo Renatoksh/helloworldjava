1222977b677af1d1b5009c57d4f53a9feb70f1f1
Renato <95065208+Renatoksh@users.noreply.github.com>
2023-08-23 21:38:23 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-     System.out.println("Hello Students from Batch 31 WITH FOR STUDENTS RUNNIG CI automated vikas teach DevOps");
+     System.out.println("Hello, This is Renato from Batch 31 WITH FOR STUDENTS RUNNIG CI automated vikas teach DevOps");
     }
 }
