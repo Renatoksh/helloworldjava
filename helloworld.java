03e410b1b45437c2c2cd2a11392ce71c47fdc9c1
Renato <95065208+Renatoksh@users.noreply.github.com>
2023-08-23 21:31:31 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class simple{
     public static void main(String args[]){
-     System.out.println("Hello Students from Batch 31");
+     System.out.println("Hello Students from Batch 31 WITH FOR STUDENTS RUNNIG CI automated vikas teach DevOps");
     }
 }
